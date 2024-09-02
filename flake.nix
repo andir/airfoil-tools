c2abc2d5b02acdaa523f1fcc4f2362fff01daee9
Andreas Rammhold <andreas@rammhold.de>
2024-09-02 22:37:54 +0200

    add planformcreator2

--- flake.nix
+++ flake.nix
@@ -10,6 +10,11 @@
       flake = false;
     };
 
+    planformcreator2 = {
+      url = "github:jxjo/PlanformCreator2";
+      flake = false;
+    };
+
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
@@ -17,7 +22,7 @@
   outputs = { self, flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" ];
     perSystem = { self', pkgs, ... }: let
-      allPackages = [ self'.packages.xoptofoil2 pkgs.xflr5 self'.packages.airfoileditor ];
+      allPackages = [ self'.packages.xoptofoil2 pkgs.xflr5 self'.packages.airfoileditor self'.packages.planformcreator2 ];
     in {
       packages = {
         xoptofoil2 = pkgs.stdenv.mkDerivation {
@@ -48,6 +53,18 @@
           exec ${pythonEnv}/bin/python ${inputs.airfoileditor}/AirfoilEditor.py "$@"
         '';
 
+        planformcreator2 = let
+          pythonEnv = pkgs.python3.withPackages (p: with p; [
+            numpy
+            matplotlib
+            customtkinter
+            termcolor
+            colorama
+            ezdxf
+          ]);
+        in pkgs.writeShellScriptBin "planformcreator2" ''
+          exec ${pythonEnv}/bin/python ${inputs.planformcreator2}/PlanformCreator2.py "$@"
+        '';
         release = pkgs.symlinkJoin {
           name = "release";
           paths = allPackages;
